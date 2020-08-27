43cd5cab722a286ddaf96758e88792f951fb720c
zaidayoub993 <zsayoub@gmail.com>
2020-08-27 10:30:26 +0300

    new loop

--- js/add-content.js
+++ js/add-content.js
@@ -12,4 +12,21 @@ if (hourNow > 18) {
     greeting = 'Something went wrong!';
   }
 document.write('<h3>'+greeting+'</h3>');
-
+var age =2
+while (age < 10) {
+  console.log("yes")
+  age++;
+  age = age +2;
+}
+for(var count=1; count < 10; count=count+3
+   ){
+     //mycode
+   }
+   var number = 0;
+   while (number <= 10) {
+      number++;
+      if ((number % 2) != 0) {
+          continue;
+      }
+      alert(number);     // => 2, 4, 6, 8, 10 
+   }
